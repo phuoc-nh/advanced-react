f24f59d3e8576abf4d5bd5eaee733b124b18a607
Phuoc Nguyen <huuphuoc252513@gmail.com>
2025-06-19 12:02:12 +1000

    Type segmentation usage

--- client/src/features/users/types.ts
+++ client/src/features/users/types.ts
@@ -16,3 +16,7 @@ export type UserWithUserContext = User & {
 };
 
 export type UserForDetails = UserWithHostedExperiences & UserWithFollowCounts & UserWithUserContext;
+
+// A sense of Interface segmentation being used here
+// Only define necessary properties for each type
+// Do not wrap types in a single type declaration, this could lead to confusion and unnecessary types in components
